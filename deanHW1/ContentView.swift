803a0170d3468130718303ffefb9fba72d42017f
andy <andy870622@gmail.com>
2020-03-19 16:31:58 +0800

    Initial Commit

---
+++ deanHW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  deanHW1
+//
+//  Created by User15 on 2020/3/19.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
